fbd5d8ea16d8aba3b06cf83d2cff60290d19d050
Smrutikrushnapanda <skpanda017@gmail.com>
2026-01-07 13:43:21 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
  app.enableCors({
   origin: '*',
   credentials: true,
-});
+}); 
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
